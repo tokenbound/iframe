88f6880fce915cb67af7bda98ae11009cdfb76ae
Ray <huynh.r.k@gmail.com>
2023-06-19 09:54:38 -0700

    update env example file

--- lib/clients/viem.ts
+++ lib/clients/viem.ts
@@ -1,10 +1,7 @@
 import { createPublicClient, http } from "viem";
 import { goerli, mainnet } from "viem/chains";
 
-const providerEndpoint =
-  process.env.NEXT_PUBLIC_PROVIDER_ENDPOINT ||
-  "https://goerli.infura.io/v3/db79b36584e44ab09511ad3a8b6e3740";
-
+const providerEndpoint = process.env.NEXT_PUBLIC_PROVIDER_ENDPOINT || "";
 const chain = process.env.NEXT_PUBLIC_CHAIN_ID === "1" ? mainnet : goerli;
 
 export const publicClient = createPublicClient({
