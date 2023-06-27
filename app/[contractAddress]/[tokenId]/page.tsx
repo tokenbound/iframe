35ff2ca3ec305dc739f0e8cbacddd30b54a41477
Raymond Huynh <huynh.r.k@gmail.com>
2023-06-27 15:02:11 -0700

    Refactor to make custom implementation easier. (#3)
    * Update to allow fetching by custom or alchemy
    
    * add temp loading
    
    * loading state
    
    * remove console.logs

--- app/[contractAddress]/[tokenId]/page.tsx
+++ app/[contractAddress]/[tokenId]/page.tsx
@@ -4,13 +4,14 @@ import { useEffect, useState } from "react";
 import useSWR from "swr";
 import { isNil } from "lodash";
 import { getAccount, getAccountStatus, getLensNfts, getNfts } from "@/lib/utils";
-import { alchemy, rpcClient } from "@/lib/clients";
-import { Exclamation } from "@/components/icon";
+import { rpcClient } from "@/lib/clients";
+import { Exclamation, TbLogo } from "@/components/icon";
 import { Tooltip } from "@/components/ui";
-import { useGetApprovals } from "@/lib/hooks";
+import { useGetApprovals, useNft } from "@/lib/hooks";
 import { TbaOwnedNft } from "@/lib/types";
 import { TokenBar } from "./TokenBar";
 import { getAddress } from "viem";
+import { HAS_CUSTOM_IMPLEMENTATION } from "@/lib/constants";
 
 interface TokenParams {
   params: {
@@ -28,46 +29,16 @@ export default function Token({ params, searchParams }: TokenParams) {
   const [lensNfts, setLensNfts] = useState<TbaOwnedNft[]>([]);
   const [tokenInfoTooltip, setTokenInfoTooltip] = useState(false);
   const { tokenId, contractAddress } = params;
-  // const { apiEndpoint } = searchParams;
 
-  // const { data: nftData } = useNft({
-  //   tokenId: parseInt(tokenId as string),
-  //   apiEndpoint,
-  // });
-
-  // let nftDataArray: string[] = [];
-  // if (nftData && Array.isArray(nftData)) nftDataArray = nftData;
-  // if (nftData && !Array.isArray(nftData)) nftDataArray = [nftData];
-
-  // // Make sure all images are loaded before displaying it on the DOM.
-  // useEffect(() => {
-  //   if (nftData !== null) {
-  //     const imagePromises = nftDataArray.map((src: string) => {
-  //       return new Promise((resolve, reject) => {
-  //         const image = new Image();
-  //         image.onload = resolve;
-  //         image.onerror = reject;
-  //         image.src = src;
-  //       });
-  //     });
-
-  //     Promise.all(imagePromises)
-  //       .then(() => {
-  //         setImagesLoaded(true);
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error loading images:", error);
-  //       });
-  //   }
-  // }, [nftData]);
-
-  const { data: nftMetadata } = useSWR(`nft/metadata/${contractAddress}/${tokenId}`, () => {
-    return alchemy.nft.getNftMetadataBatch([{ contractAddress, tokenId }]);
+  const { data: nftMetadata, loading: nftMetadataLoading } = useNft({
+    tokenId: parseInt(tokenId as string),
+    contractAddress: params.contractAddress as `0x${string}`,
+    hasCustomImplementation: HAS_CUSTOM_IMPLEMENTATION,
   });
 
   useEffect(() => {
     if (!isNil(nftMetadata) && nftMetadata.length) {
-      const imagePromises = [nftMetadata[0]?.media[0].gateway].map((src: string) => {
+      const imagePromises = nftMetadata.map((src: string) => {
         return new Promise((resolve, reject) => {
           const image = new Image();
           image.onload = resolve;
@@ -169,8 +140,7 @@ export default function Token({ params, searchParams }: TokenParams) {
 
   return (
     <div className="w-screen h-screen bg-white">
-      <div className="relative max-h-screen mx-auto bg-black max-w-screen aspect-square overflow-hidden">
-        {/* <div className="relative max-h-screen mx-auto bg-gradient-to-b from-[#ab96d3] via-[#fbaaac] to-[#ffe8c4] max-w-screen aspect-square overflow-hidden"> */}
+      <div className="relative max-h-screen mx-auto bg-white max-w-screen aspect-square overflow-hidden">
         <div className="relative w-full h-full">
           {/* if accountDeployed is true and isLocked is false */}
           {(!isLocked || approvalData.length) && accountIsDeployed && (
@@ -191,36 +161,31 @@ export default function Token({ params, searchParams }: TokenParams) {
             tokens={tokens}
             setTokenInfoTooltip={setTokenInfoTooltip}
           />
-          <div className="relative w-full">
-            <div
-              className={`grid w-full grid-cols-1 grid-rows-1 transition ${
-                imagesLoaded ? "" : "blur-xl"
-              }`}
-            >
-              {!isNil(nftMetadata) ? (
-                <img
-                  src={`${nftMetadata[0]?.media[0].gateway}`}
-                  alt="Nft image"
-                  className="col-span-1 col-start-1 row-span-1 row-start-1 translate-x-0"
-                />
-              ) : (
-                // <div className="w-full h-full bg-gradient-to-b from-[#ab96d3] via-[#fbaaac] to-[#ffe8c4]"></div>
-                <></>
-              )}
-              {/* {!isNil(nftData) ? (
-                nftDataArray.map((layer: string, i: number) => (
-                  <img
-                    key={i}
-                    src={`${layer}`}
-                    alt="Sapienz Token Image"
-                    className="col-span-1 col-start-1 row-span-1 row-start-1 translate-x-0"
-                  />
-                ))
-              ) : (
-                // <div className="w-full h-full bg-gradient-to-b from-[#ab96d3] via-[#fbaaac] to-[#ffe8c4]"></div>
-                <></>
-              )} */}
-            </div>
+          <div className="relative w-full h-full max-w-[1080px] max-h-1080[px]">
+            {nftMetadataLoading ? (
+              <div className="h-20 w-20 absolute -translate-x-[50%] -translate-y-[60%] top-[60%] left-[50%] z-10 animate-bounce">
+                <TbLogo />
+              </div>
+            ) : (
+              <div
+                className={`grid w-full grid-cols-1 grid-rows-1 transition ${
+                  imagesLoaded ? "" : "blur-xl"
+                }`}
+              >
+                {!isNil(nftMetadata) ? (
+                  nftMetadata.map((image, i) => (
+                    <img
+                      key={i}
+                      className="col-span-1 col-start-1 row-span-1 row-start-1 translate-x-0"
+                      src={image}
+                      alt="Nft image"
+                    />
+                  ))
+                ) : (
+                  <></>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
--- lib/constants/general.ts
+++ lib/constants/general.ts
@@ -1,1 +1,4 @@
+import { nftUrl } from "@/lib/constants";
+
 export const MAX_TOKEN_ID = Number(process.env.NEXT_PUBLIC_MAX_TOKEN_ID) || 10000;
+export const HAS_CUSTOM_IMPLEMENTATION = !!nftUrl;
