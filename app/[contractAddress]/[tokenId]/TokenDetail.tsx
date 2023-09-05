b56dd280c87cebd63954a0c65df3b83e88c964c4
Devin Mancuso <devin@radelaide.net>
2023-09-04 20:38:58 -0700

    roll back

--- app/[contractAddress]/[tokenId]/TokenDetail.tsx
+++ app/[contractAddress]/[tokenId]/TokenDetail.tsx
@@ -1,8 +1,7 @@
 import { Variants, motion } from "framer-motion";
-import { GalverseLogo, TokenboundLogo } from "@/components/icon";
+import { TokenboundLogo } from "@/components/icon";
 import { Panel } from "./Panel";
 import { TbaOwnedNft } from "@/lib/types";
-import Image from "next/image";
 
 interface Props {
   className?: string;
@@ -12,34 +11,24 @@ interface Props {
   account?: string;
   tokens: TbaOwnedNft[];
   title: string;
-  chainId: number;
-  logo?: string;
-  tokenId: string;
 }
 
 const variants = {
   closed: { y: "100%", transition: { duration: 0.75 } },
-  open: { y: "0", transition: { duration: 0.75 } },
+  open: { y: "0", transition: { duration: 0.75 }, height: "85%" },
 } as Variants;
 
 const iconVariant = {
   hover: {
     opacity: 1,
+    boxShadow: "0px 1px 14px 0px rgba(0, 0, 0, 0.12)",
     transition: { duration: 0.3, ease: "easeInOut" },
   },
   unHovered: {
-    opacity: 0.7,
+    opacity: 0.3,
+    boxShadow: "none",
     transition: { duration: 0.3, ease: "easeInOut" },
   },
-};
-
-type LogoType = {
-  [logo: string]: any;
-};
-
-const Logo: LogoType = {
-  DEFAULT: TokenboundLogo,
-  GALVERSE: GalverseLogo,
 };
 
 export const TokenDetail = ({
@@ -50,39 +39,22 @@ export const TokenDetail = ({
   account,
   tokens,
   title,
-  chainId,
-  logo,
-  tokenId,
 }: Props) => {
   let currentAnimate = isOpen ? "open" : "closed";
 
-  const CustomLogo = logo ? Logo[logo.toUpperCase()] : Logo["DEFAULT"];
-
   return (
     <div className={className}>
-      {isOpen ? (
-        <Image
-          src="/close.svg"
-          alt="close-icon"
-          width={46}
-          height={46}
-          onClick={() => handleOpenClose(false)}
-          className="absolute left-4 top-4 z-10 p-2 rounded-full bg-black/60 cursor-pointer hover:bg-black transition-all duration-200 max-[440px]:h-[36px] max-[440px]:w-[36px]"
-        />
-      ) : (
-        <motion.div
-          className="absolute left-4 top-4 z-10 p-2 rounded-full bg-white/40 cursor-pointer max-[440px]:p-[6px]"
-          whileHover="hover"
-          variants={iconVariant}
-          initial="unHovered"
-        >
-          <CustomLogo onClick={() => handleOpenClose(true)} />
-        </motion.div>
-      )}
-
+      <motion.div
+        className="absolute top-4 left-4 z-10 rounded-full"
+        whileHover="hover"
+        variants={iconVariant}
+        initial="unHovered"
+      >
+        <TokenboundLogo onClick={() => handleOpenClose(!isOpen)} />
+      </motion.div>
       {isOpen && (
         <motion.div
-          className={`custom-scroll absolute bottom-0 z-10 w-full max-w-[1080px] overflow-y-auto h-[85%] max-[440px]:h-[82%]`}
+          className={`absolute max-w-[1080px] z-10 w-full bottom-0 overflow-y-auto custom-scroll`}
           animate={currentAnimate}
           variants={variants}
           initial="closed"
@@ -92,8 +64,6 @@ export const TokenDetail = ({
             account={account}
             tokens={tokens}
             title={title}
-            chainId={chainId}
-            tokenId={tokenId}
           />
         </motion.div>
       )}
