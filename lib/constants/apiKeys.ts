396546f63835e377131f2b6a9b10b85971259624
ChrisFox.eth <chris.b.fox1@gmail.com>
2023-11-21 01:54:00 -0500

    Reverting apiKey back to env variable

--- lib/constants/apiKeys.ts
+++ lib/constants/apiKeys.ts
@@ -1,2 +1,2 @@
 export const nxyzApiKey = process.env.NEXT_PUBLIC_NXYZ_API_KEY || "";
-export const graphApiKey = "4d94d723fd4f173a3415a9fb198c0f83" || "";
+export const graphApiKey = process.env.NEXT_PUBLIC_GRAPH_API_KEY || "";
